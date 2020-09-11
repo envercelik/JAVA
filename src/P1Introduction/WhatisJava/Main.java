2ce8ee418726f3d337348727c35ca68b27e2d142
Enver Çelik <devcelikenver@gmail.com>
2020-09-11 22:45:13 +0300

    Introduction - What is Java ?

---
+++ src/P1Introduction/WhatisJava/Main.java
@@ -0,0 +1,38 @@
+package P1Introduction.WhatisJava;
+
+public class Main {
+}
+
+/*
+
+Java : Nesne yönelimli , platform bağımsız , yüksek seviyeli bir programlama dilidir. Aynı zamanda bir
+       teknoloji topluluğudur. Java ile consol ,gui , web , mobil app , oyun , gömülü sistem vb. bir çok
+       alanda çalışma yapılabilir.
+
+       -Java da herşey birer nesnedir. Ve her nesnenin bir referansı bulunur. (pointer yok)
+
+       -Java ilk çıktığından beri sürekli yeni class ve kütüphaneler eklenmektedir. Bu sayede tekerleği
+       yeniden icat etmek yerine hazır sınıflar tercih edilebilir.
+
+       -Bir java uygulaması geliştirmek için jdk (java development kit) gereklidir. Jdk içerisinde daha
+       önceden yazılmış bir çok sınıf ve kütüphane vardır. Ayrıca java uygulamalarını derleyip çalıştırmamızı
+       sağlayan jre (java runtime environment) ve jvm (java virtual machine) bulunur.
+
+
+
+Çalışma Sırası :  -Java kodları .java uzantılı dosyalara yazılır.
+                  -Bu dosyalar derlenerek byte koda dönüştürülür ve uzantıları .class olur.
+                  -Byte byte kodlar jvm (java virtual machine) tarafından yorumlanarak işletim sistemine
+                   uygun komutlara dönüştürülür.
+
+
+
+
+Platform Bağımsızlığı : Javaya platform bağımsızlığı kazandıran yapı jvm dir. Kaynak kod bir kere yazılır ve
+                        her makinede çalışır çünkü her makinede ara katman olarak jvm vardır.
+
+                        -Java kaynak kodları derlenir ve byte koda dönüştürülür.
+                        -Jvm byte kodları alır hangi makinedeyse o makinenin anlayacağı şekle dönüştürüp çalıştırır.
+
+
+ */
