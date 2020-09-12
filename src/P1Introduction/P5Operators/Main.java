062e369e6605e0d2018a9811177ceae7329b6066
Enver Çelik <devcelikenver@gmail.com>
2020-09-12 15:17:50 +0300

    Introduction - Operators

---
+++ src/P1Introduction/P5Operators/Main.java
@@ -0,0 +1,51 @@
+package P1Introduction.P5Operators;
+
+public class Main {
+}
+
+
+/*Operator :
+ -Bir veya daha fazla operand alır . İilemden geçirir ve sonucu döner.
+ 9-5 = 1    burda - operator 9 ve 5 değeri operand 1 ise sonuctur.
+
+
+
+Atama     :    =
+
+ARITMETİK :  + , -  , *  , / , %
+
+ARTTIRMA  : ++
+AZALTMA   : --
+
+
+ARITMETIK ATAMA OPERATORLERİ :
+
++=
+-=
+*=
+/=
+%=
+
+
+Karşılatırma operatörleri :
+
+==
+!=
+<
+<=
+>
+>=
+
+
+Mantıksal Operatorler :
+
+||
+&&
+!
+
+Ternary :
+
+?:
+
+
+*/
